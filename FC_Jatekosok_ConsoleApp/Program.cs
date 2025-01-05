ee8eaa687306f24b80b516765ce6b99ccf17043f
ZeonRad <83959787+ZeonRad@users.noreply.github.com>
2025-01-05 22:01:47 +0100

    Add files via upload

---
+++ FC_Jatekosok_ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using FC_ClassLibrary;
+DataStore Data = DataStore.CreateCSV();
+
+Console.WriteLine($"5.feladat: {Data.Ballabas()} bal lábas játékos található.");
+
+Console.Write("6. feladat: Játékos neve: ");
+string? nev = Console.ReadLine();
+Console.WriteLine(Data.JatekosAdatlapja(nev));
+
+Console.WriteLine("7. feladat: A 4 legtöbb átlagos értékkel rendelkező játékos:");
+Console.WriteLine(Data.Top4());
